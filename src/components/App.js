27b43e139b3fa5ea50aa1ac8d37b66d50d7ad771
Siddharth Hota <siddharthhota26@gmail.com>
2023-06-19 00:43:52 +0530

    major changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,19 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 import Flames from "./Flame";
 
-class App extends Component {
-    render() {
+const App = () => {
+    const [answer, setAnswer] = useState("");
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-               <Flames></Flames>
-            </div>
-        )
-    }
+    return (
+        <div id="main" >
+            {/* Do not remove the main div */}
+            < Flames
+                setAnswer={setAnswer}
+            />
+            <h3 data-testid="answer">{answer}</h3>
+        </div>
+    )
 }
 
 
--- src/components/Flame.js
+++ src/components/Flame.js
@@ -1,72 +1,65 @@
 import React, { useState } from "react";
 
 
-const Flames = () => {
-    let [fname, setFname] = useState('');
-    let [sname, setSname] = useState('');
-    let [output, setOutput] = useState('');
+const Flames = (props) => {
+    let [name1, setName1] = useState("");
+    let [name2, setName2] = useState("");
 
-    function match() {
-        if (fname === '' || sname === '') {
-            setOutput("Please Enter valid input");
-            return;
-        }
-        let firstName = fname;
-        let secondName = sname; 
+    let { setAnswer } = props;
 
-        for (let i = 0; i < firstName.length; i++) {
-            for (let j = 0; j < secondName.length; j++) {
-                if (firstName.charAt(i) == secondName.charAt(j)) {
-                    firstName = firstName.replace(firstName.charAt(i), "");
-                    secondName = secondName.replace(secondName.charAt(j), "");
-                    i--;
+    function calculateRelation() {
+        if (name1.length === 0 || name2.length === 0) {
+            setAnswer("Please Enter valid input");
+        } else {
+            let firstName = name1;
+            let secondName = name2;
+
+            for (let i = 0; i < firstName.length; i++) {
+                for (let j = 0; j < secondName.length; j++) {
+                    if (firstName.charAt(i) == secondName.charAt(j)) {
+                        firstName = firstName.replace(firstName.charAt(i), "");
+                        secondName = secondName.replace(secondName.charAt(j), "");
+                        i--;
+                    }
                 }
             }
-        }
 
-        switch((firstName.length + secondName.length) % 6){
-            case 0:
-                setAnswer("Siblings");
-                break;
-            case 1:
-                setAnswer("Friends");
-                break;
-            case 2:
-                setAnswer("Love");
-                break;
-            case 3:
-                setAnswer("Affection");
-                break;
-            case 4:
-                setAnswer("Marriage");
-                break;
-            case 5:
-                setAnswer("Enemy");
-                break;
-            default:
-                setAnswer("");
+            switch ((firstName.length + secondName.length) % 6) {
+                case 0:
+                    setAnswer("Siblings");
+                    break;
+                case 1:
+                    setAnswer("Friends");
+                    break;
+                case 2:
+                    setAnswer("Love");
+                    break;
+                case 3:
+                    setAnswer("Affection");
+                    break;
+                case 4:
+                    setAnswer("Marriage");
+                    break;
+                case 5:
+                    setAnswer("Enemy");
+                    break;
+                default:
+                    setAnswer("");
+            }
         }
     }
-
-    function clear() {
-        setFname('');
-        setSname('');
-        setOutput('');
-    }
-
 
     return (
-        <div>
-            <input data-testid="input1" placeholder="Enter first name" name="name1" value={fname} onChange={(e) => { setFname(e.target.value.toLowerCase()) }}></input>
-            <input data-testid="input2" placeholder="Enter second name" name="name2" value={sname} onChange={(e) => { setSname(e.target.value.toLowerCase()) }}></input>
-            <button data-testid="calculate_relationship" onClick={match}>Calculate Relationship Future</button>
-            <button data-testid="clear" onClick={clear}>Clear</button>
-
-            <h3 data-testid="answer">{output}</h3>
-            {/* { 
-                 output &&
-            } */}
-        </div>
+        <form onSubmit={(e) => { e.preventDefault() }}>
+            <input type="text" name="name1" value={name1} data-testid="input1" onChange={(e) => { setName1(e.target.value) }} placeholder='Enter first name' />
+            <input type="text" name="name2" value={name2} data-testid="input2" onChange={(e) => { setName2(e.target.value) }} placeholder='Enter second name' />
+            <button type="submit" data-testid="calculate_relationship" onClick={calculateRelation}>Calculate Relationship Future</button>
+            <button data-testid="clear" onClick={() => {
+                setName1("");
+                setName2("");
+                setAnswer("");
+            }}>Clear</button>
+        </form>
     )
 }
 
